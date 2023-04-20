0941fe8b69a1c268ce4c635f891bc449624e7f97
Dmitriy Knyazev <knyazev147@gmail.com>
2023-04-20 17:52:58 +0300

    task 1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -4,6 +4,15 @@ function addInCartMessage(title, price) {
 
     // Задание №1.1. Оповещение о добавлении в корзину
 
+    let message;
+message = productName;
+message = message + " ";
+message = message + "за";
+message = message + " ";
+message += productPrice;
+message = message + " ";
+message = message + "теперь в корзине!";
+console.log(message);
     // создайте переменную message
     // присвойте переменной message значение переменной productName
     // добавьте в конец переменной message пробел
@@ -23,6 +32,10 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+    let newValue;
+    newValue = oldValue;
+    newValue = newValue+1;
+    console.log(newValue);
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
@@ -36,7 +49,11 @@ function addInCartCountChange(value) {
 function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
-
+    let newSum = oldSum + difference;
+    let newSumText = newSum + ' ₽';
+    newSumText = `${newSum} ₽`;
+    console.log(newSum);
+    console.log(newSumText);
     // Задание №1.3. Увеличение суммы в корзине
 
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
